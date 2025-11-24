93b0a5ae29de0b18076b0c6f94e775345fcfc056
Fahad-Farabee <smfahadfarabee@gmail.com>
2025-11-24 18:48:10 +0600

    useState hook

--- first-react-project/src/App.jsx
+++ first-react-project/src/App.jsx
@@ -1,9 +1,14 @@
+import { useState } from "react";
 import "./App.css";
 
 const Card = ({ title }) => {
+  const [hasLiked, setHasLiked] = useState(false);
   return (
     <div className="card">
       <h2>{title}</h2>
+      <button onClick={() => setHasLiked(!hasLiked)}>
+        {hasLiked ? "liked" : "like"}
+      </button>
     </div>
   );
 };
