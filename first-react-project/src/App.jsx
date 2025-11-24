91070adc84e9c7e156487e66a9dcc137ee9cdb6d
Fahad-Farabee <smfahadfarabee@gmail.com>
2025-11-24 16:30:47 +0600

    components

---
+++ first-react-project/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+const Btn = () => {
+  return <button>click me</button>;
+};
+
+const App = () => {
+  return (
+    <div>
+      <h2>Function arrow component</h2>
+      <Btn />
+    </div>
+  );
+};
+
+export default App;
